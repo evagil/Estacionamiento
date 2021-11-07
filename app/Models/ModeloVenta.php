cdd37ffbb4f2f4f27cedaf12127fdc7331567c56
Emy-m <emiunrn@gmail.com>
2021-11-07 19:47:52 -0300

    Fusion de modelos Venta y AutosEstacionados

--- app/Models/ModeloVenta.php
+++ app/Models/ModeloVenta.php
@@ -12,14 +12,15 @@ class ModeloVenta extends Model
     protected $returnType     = 'App\Entities\Venta';
     protected $allowedFields = ['hora_inicio','hora_fin','cantidad_horas','monto','id_usuario','id_auto','id_zona_horario','vender'];
 
-   
     public function listarVentas($id){
-       
         return $this->where("ventas.id_auto = $id")->findAll();
-       
     }
 
-   
-
-
+    public function encontrarVehiculosEstacionados()
+    {
+        return $this->select('usuarios.nombre as nombre_usuario, usuarios.apellido, autos.*, zonas.*, ventas.*')
+            ->join('usuarios','ventas.id_usuario= usuarios.id_usuario')
+            ->join('autos','autos.id_auto=ventas.id_auto')
+            ->join('zonas','zonas.id_zona=ventas.id_zona_horario')->findAll();
+    }
 }
