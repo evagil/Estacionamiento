e2fe44e846edf1ddfca720c7b251157c2faa7171
Emy-m <emiunrn@gmail.com>
2021-11-17 15:37:33 -0300

    Inspector: Inspeccionar Estadia Remake

--- app/Views/inspectores/formulario.php
+++ app/Views/inspectores/formulario.php
@@ -1,49 +1,12 @@
-<br>
-
-
-
-<form method="post" action="<?= base_url('usuarios/inspectores/envioPost') ?>">
-
-    <?php if (isset($validacion) && $validacion->hasError('patente')) { ?>
-        <span class="text-danger"> <?= "*".$validacion->getError('patente'); ?> </span>
-    <?php } ?>
-    <div>
-        <input type="text" name="valor1" placeholder="patente">
-    </div>
-    <br>
-    <br>
-
-
-    <div style="text-align: left">
-            <button type="submit" class="btn btn-primary">Buscar</button>
-    </div>
-
-
-
-   
-    
-</form>
-
-
-
-<script type="text/javascript">
-    function showContent() {
-        element = document.getElementById("content");
-        check = document.getElementById("check");
-        if (check.checked) {
-            element.style.display='block';
-        }
-        else {
-            element.style.display='none';
-        }
-    }
-</script>
-
-<b>Mostrar contenido?</b>
-<input type="checkbox" name="check" id="check" value="1" onchange="javascript:showContent()" />
-
-<div id="content" style="display: none;">
-   contenido del div escondido<br/>
-   contenido del div escondido<br/>
-   contenido del div escondido<br/>
- </div>
+<div class="w-50" style="margin: auto; text-align: center">
+    <form method="post" action="<?= base_url('usuarios/inspectores/envioPost') ?>">
+        <?php if (isset($validacion) && $validacion->hasError('patente')) { ?>
+            <span class="text-danger"> <?= "*".$validacion->getError('patente'); ?> </span>
+        <?php } ?>
+        <div class="form-floating mb-3">
+            <input type="text" class="form-control" id="valor1" name="valor1" placeholder="Patente">
+            <label for="valor1">Patente</label>
+        </div>
+        <button type="submit" class="btn btn-primary">Buscar</button>
+    </form>
+</div>
