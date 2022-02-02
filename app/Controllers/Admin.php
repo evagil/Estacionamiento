860baf1f7b7c79b38cd7248d1edd1067c8877f63
Emy-m <emiunrn@gmail.com>
2022-02-02 14:46:41 -0300

    borrados los comentarios

--- app/Controllers/Admin.php
+++ app/Controllers/Admin.php
@@ -32,121 +32,119 @@ class Admin extends BaseController
     public function guardarAlta()
     {
         $usuario = new ModeloUsuario();
-        $validation =  \Config\Services::validation();
+        $validation = \Config\Services::validation();
         $user = new \App\Entities\Usuario($this->request->getPost());
 
         if ($validation->run($this->request->getPost(), 'formAdministrador')) {
             $usuario->save($user);
             return redirect()->to(base_url('usuarios/perfil'))->with('mensaje', 'Usuario creado existosamente.');
-        }
-        else {
+        } else {
             return redirect()->to(base_url('usuarios/administrador/altaUsuario'))->with('validation', $validation)->withInput();
         }
     }
 
-    public function listarUsuarios(){
+    public function listarUsuarios()
+    {
         $data['titulo'] = 'Usuarios';
-        echo view ('usuarios/perfil/perfil-header', $data);
-        echo view ('administrador/listar');
-        echo view ('usuarios/perfil/perfil-footer');
+        echo view('usuarios/perfil/perfil-header', $data);
+        echo view('administrador/listar');
+        echo view('usuarios/perfil/perfil-footer');
     }
 
-    public function listarZonas(){
+    public function listarZonas()
+    {
         $data['titulo'] = 'Zonas';
-        echo view ('usuarios/perfil/perfil-header', $data);
-        echo view ('administrador/listarZonas');
-        echo view ('usuarios/perfil/perfil-footer');
+        echo view('usuarios/perfil/perfil-header', $data);
+        echo view('administrador/listarZonas');
+        echo view('usuarios/perfil/perfil-footer');
     }
 
-    public function obtenerZonas(){
+    public function obtenerZonas()
+    {
         $modelo = new ModeloZona();
         $zonas = $modelo->obtenerDatosZonas();
         return $this->response->setJSON($zonas);
     }
 
-   public function obtenerDetalleZona($id){
-    $modelo = new ModeloZona();
+    public function obtenerDetalleZona($id)
+    {
+        $modelo = new ModeloZona();
 
-    $zonas = $modelo->obtenerDetalleZona($id);
+        $zonas = $modelo->obtenerDetalleZona($id);
 
-    return $this->response->setJSON($zonas);
-   }
-
-
-   public function editarZonas($id){
-    helper('form');
-    $modelo = new ModeloZona();
- 
-    $zona = $modelo->obtenerDetalleZona($id);
-
-    if (session()->getFlashdata('validation')) {
-        $data['validacion'] = session()->getFlashdata('validation');
+        return $this->response->setJSON($zonas);
     }
 
-    $data['titulo'] = "Editar";
-    $data['zonas'] = $zona;
+
+    public function editarZonas($id)
+    {
+        helper('form');
+        $modelo = new ModeloZona();
+
+        $zona = $modelo->obtenerDetalleZona($id);
+
+        if (session()->getFlashdata('validation')) {
+            $data['validacion'] = session()->getFlashdata('validation');
+        }
+
+        $data['titulo'] = "Editar";
+        $data['zonas'] = $zona;
 
 
-   // print_r($zona);
+        // print_r($zona);
 
-    echo view('usuarios/perfil/perfil-header', $data);
-    echo view('administrador/editarZonas', $data);
-    echo view('usuarios/perfil/perfil-footer');
-  
-   }
+        echo view('usuarios/perfil/perfil-header', $data);
+        echo view('administrador/editarZonas', $data);
+        echo view('usuarios/perfil/perfil-footer');
 
-
-   
+    }
 
 
     public function encontrarUsuarios($id = null)
     {
         $modelo = new ModeloUsuario();
 
-        if(isset($id)) {
+        if (isset($id)) {
             $usuarios = $modelo->obtenerUsuarioPorId($id);
-        }
-        else {
+        } else {
             $usuarios = $modelo->encontrarUsuarios();
         }
 
         return $this->response->setJSON($usuarios);
     }
 
-    
 
-    public function eliminar($id){
+    public function eliminar($id)
+    {
         $modelo = new ModeloUsuario();
         $modelo->bajaUsuario($id);
         return redirect()->to(base_url('usuarios/administrador/listadoUsuarios'))->with('mensaje', 'Usuario eliminado existosamente.');
     }
 
-    public function reestablecerClave($id){
+    public function reestablecerClave($id)
+    {
         $modelo = new ModeloUsuario();
         $modelo->reestablecerClave($id);
         return redirect()->to(base_url('usuarios/administrador/listadoUsuarios'))->with('mensaje', 'Clave reestablecida con exito.');
     }
 
-    public function listarVehiculosEstacionados() {
+    public function listarVehiculosEstacionados()
+    {
         $data['titulo'] = "Listado de Vehiculos Estacionados";
         echo view('usuarios/perfil/perfil-header', $data);
         echo view('administrador/listarVehiculosEstacionados');
         echo view('usuarios/perfil/perfil-footer');
     }
 
-    public function obtenerVehiculosEstacionados(){
+    public function obtenerVehiculosEstacionados()
+    {
         $ventas = new ModeloVenta();
         $autos = $ventas->listarVentas(null, null, 1);
         return $this->response->setJSON($autos);
     }
 
-<<<<<<< HEAD
-    
-    
-
-
-=======
-    public function listadoInfracciones() {
+    public function listadoInfracciones()
+    {
         helper('date');
         $data['titulo'] = "Listado de Ifracciones";
         echo view('usuarios/perfil/perfil-header', $data);
@@ -163,52 +161,40 @@ class Admin extends BaseController
         $vehiculo = null;
         $input = $this->request->getJSON(true);
 
-        if (isset($input['inspector']) && strlen($input['inspector']) > 0)
-        {
+        if (isset($input['inspector']) && strlen($input['inspector']) > 0) {
             $modeloUsuario = new ModeloUsuario();
             $inspector = $modeloUsuario->encontrarUsuarioDNI($input['inspector']);
 
-            if (!isset($inspector))
-            {
+            if (!isset($inspector)) {
                 $errores['inspector'] = 'No existe un usuario con ese DNI.';
             }
         }
 
-        if (isset($input['patente']) && strlen($input['patente']) > 0)
-        {
+        if (isset($input['patente']) && strlen($input['patente']) > 0) {
             $modeloAuto = new ModeloAuto();
             $vehiculo = $modeloAuto->obtenerAutos($input['patente']);
 
-            if (!isset($vehiculo))
-            {
+            if (!isset($vehiculo)) {
                 $errores['patente'] = 'No existe un vehiculo con esa patente.';
             }
         }
 
-        if (isset($input['desde']) && strlen($input['desde']) === 0)
-        {
+        if (isset($input['desde']) && strlen($input['desde']) === 0) {
             $input['desde'] = null;
         }
 
-        if (isset($input['hasta']) && strlen($input['hasta']) === 0)
-        {
+        if (isset($input['hasta']) && strlen($input['hasta']) === 0) {
             $input['hasta'] = null;
         }
 
-        if (count($errores) > 0)
-        {
+        if (count($errores) > 0) {
             return $this->response->setJSON(['errores' => $errores]);
-        }
-        else if (isset($idInfraccion))
-        {
+        } else if (isset($idInfraccion)) {
             $infracciones = $modelo->find($idInfraccion);
-        }
-        else
-        {
+        } else {
             $infracciones = $modelo->encontrarInfracciones($inspector, $vehiculo, $input['desde'], $input['hasta']);
         }
 
         return $this->response->setJSON(['infracciones' => $infracciones]);
     }
->>>>>>> a687fdab7c487babb668b4744c3f04445e84bdef
 }
